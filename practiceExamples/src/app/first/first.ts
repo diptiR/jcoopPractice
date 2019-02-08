8af6ff6e07d8183eddab19e4cb7f4a5bbc74ae48
unknown <Admin@Training_Laptop10.India.XoriantCorp.com>
2019-02-08 13:10:17 +0530

    First: Creating a Data-bound Component

---
+++ practiceExamples/src/app/first/first.ts
@@ -0,0 +1,20 @@
+import { Component } from "@angular/core";
+
+@Component({
+    "selector": "home",
+    "templateUrl": "./first.html"
+})
+
+export class HomeComponent {
+    data = {
+        name: 'ngConf 2025',
+        date: '3/1/2025',
+        time: '8am',
+        location:
+        {
+            address: '123 Main St',
+            city: 'Salt Lake City, UT',
+            country: 'USA'
+        }
+    }
+}
