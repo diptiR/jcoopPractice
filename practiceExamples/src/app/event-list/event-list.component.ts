bf72d83a66cc913e6151923af2c98110caca5179
Dipti Redkar <redkar_d@India.XoriantCorp.com>
2019-02-09 16:12:47 +0530

    Eight: Hiding Elements with ngIf

--- practiceExamples/src/app/event-list/event-list.component.ts
+++ practiceExamples/src/app/event-list/event-list.component.ts
@@ -10,23 +10,24 @@ export class EventListComponent {
       name: "Angular Connect",
       date: "9/26/2036",
       time: "10am",
-      location: { address: "1 London Rd", city: "London", country: "England" }
-    },
-    {
-      name: "ng-nl",
-      date: "4/15/2037",
-      time: "9am",
-      location: { address: "127 DT ", city: "Amsterdam", country: "NL" }
+      location: {
+        address: "1 London Road ",
+        city: "London",
+        country: "England"
+      }
     },
     {
       name: "ng-conf 2037",
       date: "4/15/2037",
       time: "9am",
-      location: {
-        address: "The Palatial America Hotel",
-        city: "Salt Lake City",
-        country: "USA"
-      }
+      onlineUrl: "https://www.ng-conf.org/"
+    },
+    { name: "Future Conf (Location/Url TBD)", date: "6/10/2037", time: "8am" },
+    {
+      name: "ng-nl",
+      date: "4/15/2037",
+      time: "9am",
+      onlineUrl: "http://ng-nl.org/"
     },
     {
       name: "UN Angular Summit",
@@ -36,7 +37,8 @@ export class EventListComponent {
         address: "The UN Angular Center",
         city: "New York",
         country: "USA"
-      }
+      },
+      onlineUrl: "http://unangularsummit.org"
     }
   ];
 }
